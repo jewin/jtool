460bb2c097afa0320b2194266dda462e6eb0bcf3
jianyang <jjzxjgy@126.com>
2017-09-19 09:34:21 +0800

    新增float类型的数字比较功能

--- src/main/java/com/jewin/common/util/NumberUtils.java
+++ src/main/java/com/jewin/common/util/NumberUtils.java
@@ -16,11 +16,11 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isLessThan(final BigDecimal isThis, final BigDecimal lessThanThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
 
         if( lessThanThis == null ){
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         return isThis.compareTo(lessThanThis) < 0;
@@ -34,11 +34,11 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isLessThanOrEqualTo( final BigDecimal isThis, final BigDecimal lessThanOrEqualToThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
 
         if( lessThanOrEqualToThis == null ) {
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         return isThis.compareTo( lessThanOrEqualToThis ) <= 0;
@@ -52,10 +52,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isGreaterThan( final BigDecimal isThis, final BigDecimal greaterThanThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
         if( greaterThanThis == null ){
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         return isThis.compareTo( greaterThanThis ) > 0;
@@ -69,10 +69,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isGreaterThanOrEqualTo( final BigDecimal isThis, final BigDecimal greaterThanOrEqualToThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
         if( greaterThanOrEqualToThis == null ){
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         return isThis.compareTo( greaterThanOrEqualToThis ) >= 0;
@@ -86,10 +86,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isEqualTo( final BigDecimal isThis, final BigDecimal equalToThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
         if( equalToThis == null ) {
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         return isThis.compareTo( equalToThis ) == 0;
@@ -128,7 +128,7 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      * @param bd                             the number
      * @param left                       the left one
      * @param right                      the right one
-     * @return
+     * @return boolean
      */
     public static boolean isGreaterLeftAndLessEqualRight(final BigDecimal bd, final BigDecimal left, final BigDecimal right ) {
         if( bd == null ){
@@ -220,16 +220,16 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      * the first one is less than second one
      * @param isThis                        the first one
      * @param lessThanThis                  the second one
-     * @return
+     * @return boolean
      */
     public static boolean isLessThan(final Double isThis, final Double lessThanThis ) {
 
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
 
         if( lessThanThis == null ){
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         BigDecimal isThisBD = BigDecimal.valueOf(isThis);
@@ -246,11 +246,11 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isLessThanOrEqualTo( final Double isThis, final Double lessThanOrEqualToThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
 
         if( lessThanOrEqualToThis == null ) {
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         BigDecimal isThisBD = BigDecimal.valueOf(isThis);
@@ -267,10 +267,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isGreaterThan( final Double isThis, final Double greaterThanThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
         if( greaterThanThis == null ){
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         BigDecimal isThisBD = BigDecimal.valueOf(isThis);
@@ -287,10 +287,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isGreaterThanOrEqualTo( final Double isThis, final Double greaterThanOrEqualToThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
         if( greaterThanOrEqualToThis == null ){
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
 
@@ -308,10 +308,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
      */
     public static boolean isEqualTo( final Double isThis, final Double equalToThis ) {
         if( isThis == null ){
-            throw new IllegalArgumentException( "First argument is null" );
+            throw new IllegalArgumentException( "first argument is null" );
         }
         if( equalToThis == null ) {
-            throw new IllegalArgumentException( "Second argument is null" );
+            throw new IllegalArgumentException( "second argument is null" );
         }
 
         BigDecimal isThisBD = BigDecimal.valueOf(isThis);
@@ -447,5 +447,247 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
         return isGreaterLeftAndLessRight(bdBD, leftBD, rightBD);
     }
 
+
+
+
+
+
+
+
+    /**
+     * the first one is less than second one
+     * @param isThis                        the first one
+     * @param lessThanThis                  the second one
+     * @return
+     */
+    public static boolean isLessThan(final Float isThis, final Float lessThanThis ) {
+
+        if( lessThanThis == null ){
+            throw new IllegalArgumentException( "second argument is null" );
+        }
+
+        if( isThis == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        BigDecimal isThisBD = BigDecimal.valueOf(isThis);
+        BigDecimal lessThanThisBD = BigDecimal.valueOf(lessThanThis);
+
+        return isLessThan(isThisBD, lessThanThisBD);
+    }
+
+    /**
+     * the first one is less and equal than second one
+     * @param isThis                         the first one
+     * @param lessThanOrEqualToThis          the second one
+     * @return
+     */
+    public static boolean isLessThanOrEqualTo( final Float isThis, final Float lessThanOrEqualToThis ) {
+        if( lessThanOrEqualToThis == null ) {
+            throw new IllegalArgumentException( "second argument is null" );
+        }
+
+        if( isThis == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        BigDecimal isThisBD = BigDecimal.valueOf(isThis);
+        BigDecimal lessThanOrEqualToThisBD = BigDecimal.valueOf(lessThanOrEqualToThis);
+
+        return isLessThanOrEqualTo(isThisBD, lessThanOrEqualToThisBD);
+    }
+
+    /**
+     * the first one is greater and equal than second one
+     * @param isThis                          the first one
+     * @param greaterThanThis                 the second one
+     * @return
+     */
+    public static boolean isGreaterThan( final Float isThis, final Float greaterThanThis ) {
+        if( greaterThanThis == null ){
+            throw new IllegalArgumentException( "second argument is null" );
+        }
+
+        if( isThis == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        BigDecimal isThisBD = BigDecimal.valueOf(isThis);
+        BigDecimal greaterThanThisBD = BigDecimal.valueOf(greaterThanThis);
+
+        return isGreaterThan(isThisBD, greaterThanThisBD);
+    }
+
+    /**
+     * the first one is greater and equal than second one
+     * @param isThis                          the first one
+     * @param greaterThanOrEqualToThis                 the second one
+     * @return
+     */
+    public static boolean isGreaterThanOrEqualTo( final Float isThis, final Float greaterThanOrEqualToThis ) {
+        if( greaterThanOrEqualToThis == null ){
+            throw new IllegalArgumentException( "second argument is null" );
+        }
+        if( isThis == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+
+        BigDecimal isThisBD = BigDecimal.valueOf(isThis);
+        BigDecimal greaterThanOrEqualToThisBD = BigDecimal.valueOf(greaterThanOrEqualToThis);
+
+        return isGreaterThanOrEqualTo(isThisBD, greaterThanOrEqualToThisBD);
+    }
+
+    /**
+     * the first one is equal than second one
+     * @param isThis                          the first one
+     * @param equalToThis                     the second one
+     * @return
+     */
+    public static boolean isEqualTo( final Float isThis, final Float equalToThis ) {
+        if( equalToThis == null ) {
+            throw new IllegalArgumentException( "second argument is null" );
+        }
+        if( isThis == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        BigDecimal isThisBD = BigDecimal.valueOf(isThis);
+        BigDecimal equalToThisBD = BigDecimal.valueOf(equalToThis);
+
+        return isEqualTo(isThisBD, equalToThisBD);
+    }
+
+    /**
+     * the number greater and equal than left one , and less and equal than right one
+     * @param bd                             the number
+     * @param left                           the left one
+     * @param right                          the right one
+     * @return
+     */
+    public static boolean isBetweenInRange( final Float bd, final Float left, final Float right ) {
+
+        if( left == null ){
+            throw new IllegalArgumentException( "left bound argument is null" );
+        }
+
+        if( right == null ){
+            throw new IllegalArgumentException( "right bound argument is null" );
+        }
+
+        if( isGreaterThan(left, right) ) {
+            throw new IllegalArgumentException( "left bound is greater than right bound" );
+        }
+
+        if( bd == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        BigDecimal bdBD = BigDecimal.valueOf(bd);
+        BigDecimal leftBD = BigDecimal.valueOf(left);
+        BigDecimal rightBD = BigDecimal.valueOf(right);
+
+        return isBetweenInRange(bdBD, leftBD, rightBD);
+    }
+
+
+    /**
+     * the number gather than left one , and less and equal than right one
+     * @param bd                             the number
+     * @param left                       the left one
+     * @param right                      the right one
+     * @return
+     */
+    public static boolean isGreaterLeftAndLessEqualRight(final Float bd, final Float left, final Float right ) {
+        if( left == null ){
+            throw new IllegalArgumentException( "left bound argument is null" );
+        }
+
+        if( right == null ){
+            throw new IllegalArgumentException( "right bound argument is null" );
+        }
+
+        if( bd == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        if( isGreaterThan(left, right) ) {
+            throw new IllegalArgumentException( "left bound is greater than right bound" );
+        }
+
+
+        BigDecimal bdBD = BigDecimal.valueOf(bd);
+        BigDecimal leftBD = BigDecimal.valueOf(left);
+        BigDecimal rightBD = BigDecimal.valueOf(right);
+
+        return isGreaterLeftAndLessEqualRight(bdBD, leftBD, rightBD);
+    }
+
+
+    /**
+     * the number greater and equal left one , and less than right one
+     * @param bd                             the number
+     * @param left                       the left one
+     * @param right                      the right one
+     * @return
+     */
+    public static boolean isGreaterEqualLeftAndLessRight( final Float bd, final Float left, final Float right ) {
+
+        if( left == null ){
+            throw new IllegalArgumentException( "left bound argument is null" );
+        }
+
+        if( right == null ){
+            throw new IllegalArgumentException( "right bound argument is null" );
+        }
+
+        if( bd == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        if( isGreaterThan(left, right) ) {
+            throw new IllegalArgumentException( "left bound is greater than right bound" );
+        }
+
+        BigDecimal bdBD = BigDecimal.valueOf(bd);
+        BigDecimal leftBD = BigDecimal.valueOf(left);
+        BigDecimal rightBD = BigDecimal.valueOf(right);
+
+        return isGreaterEqualLeftAndLessRight(bdBD, leftBD, rightBD);
+    }
+
+
+    /**
+     * the number greater left one , and less than right one
+     * @param bd                             the number
+     * @param left                           the left one
+     * @param right                          the right one
+     * @return
+     */
+    public static boolean isGreaterLeftAndLessRight( final Float bd, final Float left, final Float right ) {
+
+        if( left == null ){
+            throw new IllegalArgumentException( "left bound argument is null" );
+        }
+
+        if( right == null ){
+            throw new IllegalArgumentException( "right bound argument is null" );
+        }
+
+        if( bd == null ){
+            throw new IllegalArgumentException( "first argument is null" );
+        }
+
+        if( isGreaterThan(left, right) ) {
+            throw new IllegalArgumentException( "left bound is greater than right bound" );
+        }
+
+        BigDecimal bdBD = BigDecimal.valueOf(bd);
+        BigDecimal leftBD = BigDecimal.valueOf(left);
+        BigDecimal rightBD = BigDecimal.valueOf(right);
+
+        return isGreaterLeftAndLessRight(bdBD, leftBD, rightBD);
+    }
 
 }
